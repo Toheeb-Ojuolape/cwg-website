ee98e4f682412ff5c489a7ca846c91053c0ea91b
Emmanuella Ezeh <myrealface1@gmail.com>
2023-07-04 16:53:21 +0100

    Data Center Colocation page done

---
+++ src/routes/services/colocation/BenefitingIndustriesCards.svelte
@@ -0,0 +1,107 @@
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/banking-and-finance.jpg" alt="banking-and-finance">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Banking and Finance</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Banking and finance institutions need secure and reliable data centers to
+                store and process sensitive financial data.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/telecommunications.jpg" alt="telecommunications">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Telecommunications</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Telecommunication companies require robust data centers to host their
+                network infrastructure and provide services to their customers.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/healthcare.jpg" alt="healthcare">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Healthcare</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Healthcare organizations need to store and process sensitive patient data, including
+                electronic health records, which require secure and reliable data center facilities.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/ecommerce.jpg" alt="ecommerce">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">E-commerce</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				E-commerce companies require secure and reliable data centers to store their
+                customer data and host their online stores.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/education.jpg" alt="education">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Education</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Educational institutions require secure and reliable data centers to host their
+                learning management systems, student information systems, and other educational
+                technology tools.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/energy.jpg" alt="energy">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Energy and utilities</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Energy and utility companies require secure and reliable data centers to
+                manage their operations, including power generation, transmission, and
+                distribution.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/government.jpg" alt="government">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Government</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				Government agencies require secure and reliable data centers to store and
+                process sensitive information related to national security, public safety, and
+                citizen services.
+			</p>
+		</div>
+	</div>
+</div>
