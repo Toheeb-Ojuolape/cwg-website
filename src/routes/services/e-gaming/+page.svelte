21163dbee12d8191813a5c3f645d570e0ddfac49
Emmanuella Ezeh <myrealface1@gmail.com>
2023-07-05 16:03:26 +0100

    E-gaming page done

---
+++ src/routes/services/e-gaming/+page.svelte
@@ -0,0 +1,107 @@
+<script>
+    import "../services-styles.css"
+</script>
+
+<main>
+    <header
+		class="header h-[300px] lg:h-[477px] text-white mx-auto px-4 lg:px-8 flex flex-col justify-center gap-[10px]"
+	>
+		<div class="box-container">
+            <div class="header-text-wrapper">
+                <div class="header-top-title uppercase">our product</div>
+                <h1 class="text-headline-2 header-title lg:text-headline-1">
+                    E-Gaming
+                </h1>
+
+                <p class="mb-9 header-title-desc">
+                    CWG's E-Gaming Services offer a comprehensive suite of products and services that drive
+                    the growth of the online gaming industry while promoting responsible gaming practices.
+                </p>
+            </div>
+        </div>
+	</header>
+
+    <div id="sections-wrapper" class="py-5">
+        <section class="section-container grid-content-box p-10">
+            <div class="section-text-side">
+                <h2 class="text-headline-4 lg:text-headline-2">
+                    Providing a comprehensive suite of products and support for the online gaming industry.
+                </h2>
+                <p class="text-body-s font-light lg:text-body-l my-2">
+                    CWG's e-Gaming Services is a cutting-edge solution designed to cater to the needs
+                    of the rapidly growing online gaming industry. This service provides a comprehensive
+                    suite of products and services that enable gaming companies to offer their customers
+                    a seamless and immersive gaming experience.
+                </p>
+                <p class="text-body-s font-light lg:text-body-l my-2">
+                    The e-Gaming Services offering includes gaming platforms, game development, and
+                    customization services, as well as a range of back-end support services such as
+                    payment processing, customer service, and fraud detection. CWG's e-Gaming platform
+                    is designed to be highly flexible, allowing gaming companies to easily integrate it
+                    with their existing systems and tailor it to their specific requirements.
+                </p>
+            </div>
+            <div class="section-img-side pt-2">
+                <img src="/images/gaming-with-laptop.jpg" alt="gaming-with-laptop" class="section-img">
+            </div>
+        </section>
+
+        <hr class="gray-line" />
+
+        <section class="section-container grid-content-box p-10">
+            <div class="section-title-side">
+                <h2 class="text-headline-4 lg:text-headline-2">
+                    CWG's e-Gaming Services fuel online gaming growth with customizable products
+                    for seamless experiences.
+                </h2>
+            </div>
+            <div class="section-txt-side">
+                <p class="text-body-s font-light lg:text-body-l my-2">
+                    The impact of CWG's e-Gaming Services is significant, particularly in the context of
+                    the booming online gaming industry. Online gaming is a rapidly growing sector, with
+                    a global market size estimated to reach $293 billion by 2027. The demand for online
+                    gaming services is being driven by a number of factors, including increasing internet
+                    penetration, growing smartphone adoption, and the popularity of competitive gaming.
+                </p>
+                <p class="text-body-s font-light lg:text-body-l my-2">
+                    By providing a comprehensive suite of products and services that enable gaming companies
+                    to offer a seamless and immersive gaming experience, CWG's e-Gaming Services are helping
+                    to drive the growth of the online gaming industry. The platform is designed to be highly
+                    customizable and scalable, allowing gaming companies to easily adapt to changing market
+                    conditions and customer preferences.
+                </p>
+            </div>
+        </section>
+
+        <section class="box-container mx-auto py-10">
+            <div class="flex flex-col sm:flex-row bg-whitish-blue dark:bg-dark-highlight items-center sm:gap-19">
+                <div class="flex-1">
+                    <img src="/images/happy-men.jpg" alt="happy-men">
+                </div>
+                <div class="flex-1 p-5 pb-8 sm:p-0">
+                    <p class="text-body-m max-w-[515px] w-full my-2">
+                        Moreover, CWG's e-Gaming Services are helping to promote responsible gaming practices.
+                        The platform includes features such as age verification and self-exclusion tools, which
+                        are designed to help prevent minors from accessing gambling sites and to help
+                        individuals who may be at risk of developing a gambling addiction.
+                    </p>
+                    <p class="text-body-m max-w-[515px] w-full my-2">
+                        In summary, CWG's e-Gaming Services are helping to drive the growth of the online
+                        gaming industry, while promoting responsible gaming practices. With a comprehensive
+                        suite of products and services that enable gaming companies to offer a seamless and
+                        immersive gaming experience, this solution is poised to play a key role in the future
+                        of online gaming.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+<style>
+    header.header {
+		background-image: url('/images/e-gaming-banner.jpg');
+		background-size: cover;
+        background-position: center 20%;
+	}
+</style>
