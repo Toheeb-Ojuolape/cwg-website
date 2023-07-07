5967a5f2c93895e60b7caa71975a2b09680c1044
Emmanuella Ezeh <myrealface1@gmail.com>
2023-07-07 05:25:44 +0100

    Payment Terminal Solution page done

---
+++ src/routes/services/payment-terminal-solution/PtsBenefits.svelte
@@ -0,0 +1,84 @@
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/increased-sales.jpg" alt="increased-sales">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Increased Sales and Revenue</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				By accepting electronic payments through CWG's Payment Terminal Solution, businesses
+                can expand their customer base and increase sales. The convenience and security of
+                electronic payment options attract customers and encourage repeat business, resulting
+                in higher transaction volumes and improved revenue streams.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/customer-experience.jpg" alt="customer-experience">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Enhanced Customer Experience</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				The seamless and user-friendly interface of CWG's Payment Terminal Solution improves the
+                overall customer experience. Customers can make payments quickly and securely, reducing
+                wait times and enhancing satisfaction. The solution's compatibility with various payment
+                methods also offers flexibility and convenience, catering to diverse customer preferences.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/operational-efficiency.jpg" alt="operational-efficiency">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Improved Operational Efficiency</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				CWG's Payment Terminal Solution streamlines payment processing, reducing manual
+                errors and administrative tasks. Businesses can automate transaction reconciliation, generate
+                detailed reports, and simplify financial management. This efficiency saves time, minimizes
+                paperwork, and allows businesses to focus on core operations.
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/robust-security.jpg" alt="robust-security">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Robust Security and Compliance</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				CWG's Payment Terminal Solution prioritizes the security of payment transactions, protecting
+                businesses and their customers from fraud and data breaches. By adhering to industry security
+                standards and compliance regulations, businesses can maintain trust, safeguard sensitive
+                data, and mitigate the risks associated
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="pt-4 bg-whitish-blue dark:bg-dark-highlight flex flex-col industry-block">
+	<div class="flex-1 px-5 pb-15">
+		<div class="img-box-wrapper w-full bg-vivid-burgundy mb-4">
+            <img src="/images/impact-5.jpg" alt="impact-5">
+        </div>
+		<h4 class="text-headline-6 mb-3 industry-block-title">Data-Driven Insights</h4>
+		<div class="text-body-m industry-block-desc">
+			<p>
+				The analytics and reporting capabilities of CWG's Payment Terminal Solution provide businesses
+                with valuable insights into their payment transactions. By analyzing transaction data, businesses
+                can identify trends, monitor performance, and make informed decisions to drive growth and
+                optimize their operations.
+			</p>
+		</div>
+	</div>
+</div>
