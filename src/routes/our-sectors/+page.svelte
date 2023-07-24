4b95e5bdf3adfb30dbe95adc68c2f4d16317200c
Michael Olawale <tech@mykelcodes.com>
2023-07-24 15:21:50 +0100

    sectors page

--- src/routes/our-sectors/+page.svelte
+++ src/routes/our-sectors/+page.svelte
@@ -1,46 +1,52 @@
-<script>
-    import '../services/services-styles.css';
-    import './sectors-style.css'
-    import MoreAboutUs from '$lib/components/MoreAboutUs.svelte';
+<script lang="ts">
+	import '../services/services-styles.css';
+	import './sectors-style.css';
+	import MoreAboutUs from '$lib/components/MoreAboutUs.svelte';
 	import SectorsWeServe from './SectorsWeServe.svelte';
 	import CwgPolices from './CwgPolices.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const { header, sectors, sectors_section_title, policies_section_title, policies } =
+		data.sectorsPage;
 </script>
 
 <main>
-    <header
+	<header
 		class="header h-[300px] lg:h-[477px] text-white flex flex-col justify-center gap-[10px]"
 	>
 		<div class="section-container">
 			<div class="header-text-wrapper">
-				<h1 class="text-headline-2 header-title lg:text-headline-1">Our Sectors </h1>
+				<h1 class="text-headline-2 header-title lg:text-headline-1">{header.title}</h1>
 
 				<p class="mb-9 header-title-desc">
-					CWG has experience across many industries. Our group brings each client a combination
-                    of deep industry knowledge, wide array of IT services and innovative solutions.
+					{header.sub_title}
 				</p>
 			</div>
 		</div>
 	</header>
 
-    <section class="section-container py-20 bg-whitish-blue dark:bg-dark-highlight">
-        <div class="section-header">
-            <h2 class="text-headline-2 mb-4">Sectors we serve</h2>
-        </div>
-        <div class="sectors-we-serve-wrapper grid md:grid-cols-2 gap-[30px] md:gap-[40px]">
-            <SectorsWeServe />
-        </div>
-    </section>
+	<section class="section-container py-20 bg-whitish-blue dark:bg-dark-highlight">
+		<div class="section-header">
+			<h2 class="text-headline-2 mb-4">{sectors_section_title}</h2>
+		</div>
+		<div class="sectors-we-serve-wrapper grid md:grid-cols-2 gap-[30px] md:gap-[40px]">
+			<SectorsWeServe {sectors} />
+		</div>
+	</section>
 
-    <section class="section-container py-20">
-        <div class="section-header text-center">
-            <h2 class="text-headline-2 mb-4">CWG’s Policies</h2>
-        </div>
-        <div class="cwg-policies-wrapper grid md:grid-cols-2 lg:grid-cols-4 gap-[30px] md:gap-[40px] mt-10">
-            <CwgPolices />
-        </div>
-    </section>
+	<section class="section-container py-20">
+		<div class="section-header text-center">
+			<h2 class="text-headline-2 mb-4">{policies_section_title}</h2>
+		</div>
+		<div
+			class="cwg-policies-wrapper grid md:grid-cols-2 lg:grid-cols-4 gap-[30px] md:gap-[40px] mt-10"
+		>
+			<CwgPolices policies={policies.data} />
+		</div>
+	</section>
 
-    <MoreAboutUs />
+	<MoreAboutUs />
 </main>
 
 <style>
--- src/routes/our-sectors/SectorsWeServe.svelte
+++ src/routes/our-sectors/SectorsWeServe.svelte
@@ -1,104 +1,30 @@
-<script>
-	import ArrowRightLong from "$lib/components/Svgs/ArrowRightLong.svelte";
+<script lang="ts">
+	import { CMS_URL } from '$lib/api';
+	import ArrowRightLong from '$lib/components/Svgs/ArrowRightLong.svelte';
+	import type { Sectors } from './types';
+
+	export let sectors: Sectors;
 </script>
 
-<div class="sws-block bg-white dark:bg-black">
-    <div class="sws-img-wrapper bg-midnight-blue relative min-h-[240px]">
-        <img src="/images/financial-service-institutions.jpg" alt="financial-service-institutions" class="sws-img">
-    </div>
-    <div class="sws-block-title bg-midnight-blue text-white py-4 px-8 sm:px-10">Financial Service Institutions</div>
-    <div class="sws-block-desc relative p-8 pb-30 sm:p-10 sm:pb-30">
-        <p>
-            Globally, Financial Services Institutions are constantly undergoing technological upgrades. CWG
-            supports this drive in Financial Technology platforms and provides cloud services and software
-            solutions to about 60% of all financial institutions in Nigeria and has successfully implemented
-            and supported Finacle which is the foremost core banking application in Nigerian banking
-            system. To date, CWG has installed over 4,000 ATMs, currently provides support services and
-            manages over 2,500 of these ATMs on behalf of the banks; whilst taking its commitment to excellence
-            further through her ATM as a Service Initiative.
-        </p>
-    </div>
-    <a href="/sectors/financial-service-institutions" class="learn-more-link flex gap-5 text-lg max-w-[170px] mt-10">
-        <span>Learn more</span>
-        <div class="arrow-right">
-            <ArrowRightLong strokeClassName="stroke-blue" />
-        </div>
-    </a>
-</div>
-
-<div class="sws-block bg-white dark:bg-black">
-    <div class="sws-img-wrapper bg-midnight-blue relative min-h-[240px]">
-        <img src="/images/telecommunications-2.jpg" alt="telecommunications" class="sws-img">
-    </div>
-    <div class="sws-block-title bg-midnight-blue text-white py-4 px-8 sm:px-10">Telecommunications</div>
-    <div class="sws-block-desc relative p-8 pb-30 sm:p-10 sm:pb-30">
-        <p>
-            CWG provides managed services, IT infrastructure support and integration to major
-            telecommunication giants such as MTN and Etisalat. Also, Cloud services such as the
-            SMERP are being introduced into this sector. 50% of mobile calls are routed through
-            enterprise servers and storage and are managed and supported by CWG. CWG also provides
-            40% of telecommunications managed services.
-        </p>
-    </div>
-    <a href="/sectors/telecommunications" class="learn-more-link flex gap-5 text-lg max-w-[170px] mt-10">
-        <span>Learn more</span>
-        <div class="arrow-right">
-            <ArrowRightLong strokeClassName="stroke-blue" />
-        </div>
-    </a>
-</div>
-
-<div class="sws-block bg-white dark:bg-black">
-    <div class="sws-img-wrapper bg-midnight-blue relative min-h-[240px]">
-        <img src="/images/public-sector.jpg" alt="public-sector" class="sws-img">
-    </div>
-    <div class="sws-block-title bg-midnight-blue text-white py-4 px-8 sm:px-10">Public Sector</div>
-    <div class="sws-block-desc relative p-8 pb-30 sm:p-10 sm:pb-30">
-        <p>
-            This sector is responsible for deploying Technology Solutions to assist and support different
-            tiers of Government in their bid to improve the lives of her citizenry. CWG is responsible
-            for identifying areas within Government operations that can be improved with the integration
-            of Technology to ensure overall efficiencies and operational excellence. Our partnership with
-            the various levels of Government improves service delivery to citizens and increases contribution
-            to the GDP of the economy. Some of these CWG E-Government solutions which are adaptable in either
-            the Executive, Legislature or Judiciary arms of the Government creates avenues for revenue
-            collection, accountability and transparency, whilst ensuring excellence in operations. Our
-            involvement in the Government sector supports the CWG strategy of deploying Technology solutions
-            that enable growth.
-        </p>
-    </div>
-    <a href="/sectors/public-sector" class="learn-more-link flex gap-5 text-lg max-w-[170px] mt-10">
-        <span>Learn more</span>
-        <div class="arrow-right">
-            <ArrowRightLong strokeClassName="stroke-blue" />
-        </div>
-    </a>
-</div>
-
-<div class="sws-block bg-white dark:bg-black">
-    <div class="sws-img-wrapper bg-midnight-blue relative min-h-[240px]">
-        <img src="/images/emergent.jpg" alt="emergent" class="sws-img">
-    </div>
-    <div class="sws-block-title bg-midnight-blue text-white py-4 px-8 sm:px-10">Emergent</div>
-    <div class="sws-block-desc relative p-8 pb-30 sm:p-10 sm:pb-30 ">
-        <p>
-            The Emergent Business Sector is positioned and poised to provide ICT solutions for a wider range of
-            emerging markets. These could spring up in varying sectors spanning through the Healthcare
-            Industry, Oil and Gas, Manufacturing, Mining, Education, SMEs, Agriculture, Transportation, Retail
-            Businesses, NGOs, Real Estate, Logistics & Distribution, Professional bodies and much more.
-        </p>
-        <p>
-            The business landscape is constantly evolving as are the needs of the populace of any developing
-            economy; hence CWG’s Emergent Business Sector is constantly seeking ground breaking solutions for
-            tackling would-be or existing challenges that would invariably lead to economic growth. Technology
-            is the life stream of every economy and the emerging market sector is always ‘live’ with the need for
-            many technology platforms the business can deploy, establish or integrate to ensure economic stability.
-        </p>
-    </div>
-    <a href="/sectors/emergent" class="learn-more-link flex gap-5 text-lg max-w-[170px] mt-10">
-        <span>Learn more</span>
-        <div class="arrow-right">
-            <ArrowRightLong strokeClassName="stroke-blue" />
-        </div>
-    </a>
-</div>
+{#each sectors.data as { attributes: { description, image, slug, title, content } }}
+	<div class="sws-block bg-white dark:bg-black">
+		<div class="sws-img-wrapper bg-midnight-blue relative min-h-[240px]">
+			<img src={CMS_URL + image.data?.attributes.url} alt={title} class="sws-img" />
+		</div>
+		<div class="sws-block-title bg-midnight-blue text-white py-4 px-8 sm:px-10">
+			{description}
+		</div>
+		<div class="sws-block-desc relative p-8 pb-30 sm:p-10 sm:pb-30">
+			{@html content}
+		</div>
+		<a
+			href={`/our-sectors/${slug}`}
+			class="learn-more-link flex gap-5 text-lg max-w-[170px] mt-10"
+		>
+			<span>Learn more</span>
+			<div class="arrow-right">
+				<ArrowRightLong strokeClassName="stroke-blue" />
+			</div>
+		</a>
+	</div>
+{/each}
