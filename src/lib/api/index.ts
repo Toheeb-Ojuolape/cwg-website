248bb27b5c5adfd32bcace6a1e145530216a22a4
Michael Olawale <tech@mykelcodes.com>
2023-07-29 12:20:01 +0100

    bring back integration

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://78.141.232.122/api';
+const BASE_URL =
+	process.env.NODE_ENV !== 'production' ? 'http://78.141.232.122' : 'http://127.0.0.1:1337';
 
-export const CMS_URL = 'http://78.141.232.122';
+export const CMS_URL = BASE_URL;
 
 export const apiClient = axios.create({
-	baseURL: 'http://78.141.232.122/api',
+	baseURL: `${BASE_URL}/api`,
 	timeout: 5000
 });
 
-export const grapgqlClient = axios.create({
-	baseURL: 'http://78.141.232.122/graphql',
+export const graphqlClient = axios.create({
+	baseURL: `${BASE_URL}/graphql`,
 	timeout: 10000,
 	method: 'post'
 });
---
+++ src/lib/types/mission-page.ts
@@ -0,0 +1,79 @@
+import type { DataImage } from './common-types';
+
+export interface MissionPage {
+	header: Header;
+	mission_vision_section: ManifestoSection[];
+	visions_section: ManifestoSection[];
+	manifesto_section: ManifestoSection;
+	our_values_section: OurValuesSection;
+	culture_section_title: string;
+	culture_section_subtitle: string;
+	culture_section_body: CultureSectionBody;
+	culture_attributes_section_accordion: CultureAttributesSectionAccordion;
+	culture_attributes_section_image: DataImage;
+	why_cwg_section_image: DataImage;
+	why_cwg_section_carousel: ManifestoSection[];
+	policy_section_title: string;
+	policy_section_items: CultureAttributesSectionImage;
+}
+
+export interface CultureAttributesSectionAccordion {
+	title: string;
+	items: Item[];
+}
+
+export interface Item {
+	title: string;
+	content: null | string;
+}
+
+export interface DatumAttributes {
+	title: string;
+	subtitle: string;
+	icon: DataImage;
+	file: CultureAttributesSectionImage;
+}
+
+export interface Datum {
+	attributes: DatumAttributes;
+}
+
+export interface CultureAttributesSectionImage {
+	data: Datum[] | DataClass | null;
+}
+
+export interface DataClass {
+	attributes: DataAttributes;
+}
+
+export interface DataAttributes {
+	alternativeText: string | null;
+	url: string;
+}
+
+export interface CultureSectionBody {
+	content: string;
+	title: string;
+	image_position: string;
+	image: DataImage;
+}
+
+export interface Header {
+	title: string;
+	description: null;
+	background_image: DataImage;
+}
+
+export interface ManifestoSection {
+	title: string;
+	image_position?: string;
+	content: string;
+	image: DataImage;
+	subtitle?: null | string;
+}
+
+export interface OurValuesSection {
+	title: string;
+	subtitle: string;
+	values: ManifestoSection[];
+}
