6b44464b9f78de1c5112f79ccdd518258617b053
Emmanuella Ezeh <myrealface1@gmail.com>
2023-08-01 04:21:06 +0100

    GalleryModal done

---
+++ src/routes/insights/GalleryMedia.svelte
@@ -0,0 +1,191 @@
+<script>
+	import { title } from "process";
+
+    let mediaDetails = [
+        {
+            image: '/images/cwg-30-img.jpg',
+            title: 'CWG @ 30',
+            pics: 100
+        },
+        {
+            image: '/images/staff.jpg',
+            title: 'Staff',
+            pics: 37
+        },
+        {
+            image: '/images/management.jpg',
+            title: 'Management',
+            pics: 20
+        },
+        {
+            image: '/images/board-of-directors.jpg',
+            title: 'Board of Directors',
+            pics: 17
+        },
+        {
+            image: '/images/buildings.jpg',
+            title: 'Buildings',
+            pics: 100
+        },
+        {
+            image: '/images/cwg-brand-kit.jpg',
+            title: 'CWG Brand Kit',
+            pics: 34
+        },
+        {
+            image: '/images/gallery-logos.jpg',
+            title: 'Logos',
+            pics: 10
+        },
+        {
+            image: '/images/csr.jpg',
+            title: 'CSR',
+            pics: 100
+        },
+    ]
+
+    let currentIndex = 0;
+    let sliderImages = [
+      {
+        image: '/images/working-woman-2.jpg',
+      },
+      {
+        image: '/images/working-team.jpg',
+      },
+      {
+        image: '/images/youth-bootcamp.jpg',
+      },
+      {
+        image: '/images/working-staff.jpg',
+      },
+      {
+        image: '/images/transportation.jpg',
+      },
+    ];
+
+    let showModal = false;
+    let selectedAlbumTitle = '';
+
+    const openModal = (/** @type {string} */ albumTitle) => {
+      selectedAlbumTitle = albumTitle;
+      showModal = true;
+    };
+    const closeModal = () => {
+      showModal = false;
+    };
+</script>
+
+{#each mediaDetails as mediaDetail}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="gallery-media-block hover-scale bg-pewter-blue text-white cursor-pointer" on:click={() => openModal(mediaDetail.title)}>
+        <div class="gallery-media-img relative">
+            <img src={mediaDetail.image} alt="media" class="bg-img">
+        </div>
+        <div class="gallery-media-details p-[20px]">
+            <div class="flex justify-between mb-[25px]">
+                <span class="text-[18px]">{mediaDetail.title}</span>
+                <span class="text-[14px]">{mediaDetail.pics}pcs</span>
+            </div>
+            <button class="view-all-wrapper text-[14px] flex gap-[7px] px-[5px] pb-[5px]">
+                <span>View all</span>
+                <img src="/images/zoom-out.svg" alt="zoom-out">
+            </button>
+        </div>
+    </div>
+{/each}
+
+{#if showModal}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="modal" on:click={closeModal}>
+        <div class="slider">
+            {#each sliderImages as sliderImage, i (sliderImage.image)}
+              <div>
+                {#if i === currentIndex}
+                 <!-- svelte-ignore a11y-click-events-have-key-events -->
+                 <div class="slider-img-wrapper">
+                    <img src={sliderImage.image} alt="Slide" class="slider-img" />
+                 </div>
+                  <div class="indicators">
+                    {#each sliderImages as _, j}
+                      <!-- svelte-ignore a11y-click-events-have-key-events -->
+                      <div class="indicator {j === currentIndex ? 'active' : ''}" on:click={() => currentIndex = j}></div>
+                    {/each}
+                  </div>
+                {/if}
+              </div>
+            {/each}
+            <div class="album-title bg-midnight-blue p-5">{selectedAlbumTitle}</div>
+          </div>
+    </div>
+{/if}
+
+
+<style>
+    .gallery-media-img {
+        padding-bottom: 103%;
+    }
+    .view-all-wrapper {
+        border-bottom: 1px solid white;
+    }
+
+    .slider {
+      width: 100%;
+      overflow: hidden;
+      position: relative;
+    }
+    .slider-img-wrapper {
+        width: 100%;
+        height: 300px;
+        position: relative;
+        cursor: pointer;
+    }
+    .slider img.slider-img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }  
+    .album-title {
+      /* position: absolute;
+      bottom: 10px;
+      left: 10px; */
+      width: 100%;
+      color: white;
+      font-size: 18px;
+      cursor: pointer;
+    }
+    .indicators {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      z-index: 1;
+      margin-top: 20px;
+    }
+    .indicator {
+      width: 13px;
+      height: 13px;
+      border-radius: 50%;
+      background-color: #d1d1d1;
+      margin: 0 3px;
+      cursor: pointer;
+    }  
+    .indicator.active {
+      background-color: var(--color-midnight-blue);
+      width: 26px;
+      border-radius: 12px;
+    }
+  
+    .modal {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.7);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 999;
+      padding: 10px;
+    }
+</style>
