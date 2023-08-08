53795e57c4568282ec11be4f58c3ebbf9a1d5103
Michael Olawale <aymikey.olawale@gmail.com>
2023-08-08 22:26:01 +0100

    footer form submission

--- src/lib/components/Footer/FooterRightComponent.svelte
+++ src/lib/components/Footer/FooterRightComponent.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
+	import { applyAction, deserialize, enhance } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
 	import type { SocialMediaHandles } from '$lib/types/footer-types';
 	import ArrowRightSolidIcon from '../Svgs/ArrowRightSolidIcon.svelte';
 	import FooterLogo from '../Svgs/FooterLogo.svelte';
 	import FooterSocials from './FooterSocials.svelte';
 
 	export let socialLinks: SocialMediaHandles['data'];
+
+	let error: string | undefined;
+	let success = false;
+
+	async function handleSubmit(event: any) {
+		error = undefined;
+		success = false;
+
+		// @ts-ignore
+		const data = new FormData(this);
+
+		// @ts-ignore
+		const response = await fetch(this.action, {
+			method: 'POST',
+			body: data
+		});
+
+		const result = deserialize(await response.text());
+
+		if (result.type !== 'success') {
+			// @ts-ignore
+			error = result.data.error;
+		}
+
+		if (result.type === 'success') {
+			success = true;
+			await invalidateAll();
+		}
+
+		applyAction(result);
+	}
 </script>
 
 <div class="lg:mx-19 pt-20 lg:pt-22 max-h-fit flex flex-col justify-between lg:pb-8">
@@ -23,16 +56,34 @@
 				Subscribe to our newsletter and receive the latest technology insights and updates
 				from the strides of CWG delivered straight to your inbox.
 			</p>
-			<div class="relative">
-				<input
-					class="mb-[82px] pl-4 pr-8 focus-within:outline focus-within:outline-white text-white peer h-11 w-[300px] bg-transparent border border-black-600 placeholder:text-pewter-blue placeholder:text-button-s"
-					placeholder="My email..."
-					type="email"
-				/>
-				<div class="absolute right-2 top-[10px]">
-					<ArrowRightSolidIcon />
+			<form
+				method="POST"
+				action="/insights"
+				on:submit|preventDefault={handleSubmit}
+				class="relative"
+				use:enhance
+			>
+				<div class="mb-[82px]">
+					<input
+						class="mb-1 pl-4 pr-8 focus-within:outline focus-within:outline-white text-white peer h-11 w-[300px] bg-transparent border border-black-600 placeholder:text-pewter-blue placeholder:text-button-s"
+						placeholder="My email..."
+						type="email"
+						name="email"
+					/>
+					{#if error}
+						<span class="text-error block text-body-s">{error}</span>
+					{/if}
+
+					{#if success}
+						<span class="text-light-green block text-body-s"
+							>Successfully signed up</span
+						>
+					{/if}
+					<div class="absolute right-2 top-[10px]">
+						<ArrowRightSolidIcon />
+					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 	</div>
 
